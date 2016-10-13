d0b5aef64fb46d23f4236e6bd091d02164767acb
manishrw <manishwr@gmail.com>
2016-10-13 21:14:42 +0530

    Added AlphaNum sort to sort cards

--- sets.js
+++ sets.js
@@ -45,13 +45,36 @@ var set_load = function(set_code, callback) {
     });
 };
 
+var sortAlphaNum = function(a,b) {
+    var reA = /[^a-zA-Z]/g;
+    var reN = /[^0-9]/g;
+    var AInt = parseInt(a, 10);
+    var BInt = parseInt(b, 10);
+
+    if(isNaN(AInt) && isNaN(BInt)){
+        var aA = a.replace(reA, "");
+        var bA = b.replace(reA, "");
+        if(aA === bA) {
+            var aN = parseInt(a.replace(reN, ""), 10);
+            var bN = parseInt(b.replace(reN, ""), 10);
+            return aN === bN ? 0 : aN > bN ? 1 : -1;
+        } else {
+            return aA > bA ? 1 : -1;
+        }
+    }else if(isNaN(AInt)){//A is not an Int
+        return 1;//to make alphanumeric sort first return -1 here
+    }else if(isNaN(BInt)){//B is not an Int
+        return -1;//to make alphanumeric sort first return 1 here
+    }else{
+        return AInt > BInt ? 1 : -1;
+    }
+};
+
 var set_save = function(set, callback) {
     var setPath = path.join(__dirname, 'db', set.code + '.json');
 
     // Sort cards
-    set.cards = set.cards.sort(function(a, b) {
-	return(a.number.localeCompare(b.number));
-    });
+    set.cards = set.cards.sort(sortAlphaNum);
 
     set.cards.forEach(function(card) {
 	var aux;
